fe14f8bc756438f1d9f7b0e12f20f44a02603252
amarano <amarano@abim.org>
2016-03-30 20:04:42 -0400

    initial commit

---
+++ AntiXssString.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AntiXssString.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Script_Tags_Should_Be_Removed()
+        {
+            var s = "<script>window.alert('hello, world')</script>";
+            AntiXssString sut = s;
+            string actual = sut;
+            Assert.AreNotEqual(actual, s);
+        }
+    }
+}
---
+++ AntiXssString/AntiXssString.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+using System.Web.Security.AntiXss;
+
+namespace AntiXssString
+{
+    public class AntiXssString
+    {
+        private readonly string _s;
+
+        private AntiXssString(string s)
+        {
+            _s = s;
+        }
+
+        private string GetValue()
+        {
+            var safeString = AntiXssEncoder.HtmlEncode(_s, true);
+            return safeString;
+        }
+
+        protected string GetRawValue()
+        {
+            return _s;
+        }
+
+        public static implicit operator AntiXssString(string s)
+        {
+            return new AntiXssString(s);
+        }
+        
+        public static implicit operator string(AntiXssString s)
+        {
+            return s.GetValue();
+        }
+
+        public override string ToString()
+        {
+            return this.GetValue();
+        }
+
+    }
+}
